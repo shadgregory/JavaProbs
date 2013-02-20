90c64ed382c65e237000412eba2cefd54bfc3d0e
Shad Gregory <captshadg@gmail.com>
2013-02-19 23:49:29 -0600

    put the Euler stuff in one class

---
+++ RunEuler.java
@@ -0,0 +1,7 @@
+public class RunEuler {
+    public static void main(String args[]) {
+        Euler e = new Euler();
+        System.out.println("1. The sum is " + e.euler1());
+        System.out.println("2. The sum is " + e.euler2());
+    }
+}
