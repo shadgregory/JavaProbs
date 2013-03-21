f7a91d6897f27a92d9314a8ad8d6ab7ecad75a4a
Shad Gregory <captshadg@gmail.com>
2013-03-20 23:04:30 -0500

    Euler 9

--- src/Euler.java
+++ src/Euler.java
@@ -95,23 +95,23 @@ public class Euler {
      * numbers from 1 to 20?
      */
     public int euler5() {
-	int smallest = 20;
-	boolean foundit = false;
-	while (true) {
-	    for (int i=2;i<=20;i++) {
-		if ((smallest % i) == 0) {
-		    foundit = true;
-		    continue;
-		} else {
-		    smallest += 20;
-		    foundit = false;
-		    break;
-		}
-	    }
-	    if (foundit)
-		break;
-	}
-	return smallest;
+        int smallest = 20;
+        boolean foundit = false;
+        while (true) {
+            for (int i=2;i<=20;i++) {
+                if ((smallest % i) == 0) {
+                    foundit = true;
+                    continue;
+                } else {
+                    smallest += 20;
+                    foundit = false;
+                    break;
+                }
+            }
+            if (foundit)
+                break;
+        }
+        return smallest;
     }
 
     /* Euler 6
@@ -119,44 +119,64 @@ public class Euler {
      * and the square of the sum.
      */
     public int euler6() {
-	int sum_squares = 0;
-	int sum_numbers = 0;
-	for (int i=1;i<=100;i++) {
-	    sum_squares = sum_squares + i * i;
-	    sum_numbers = sum_numbers + i;
-	}
-	return sum_numbers * sum_numbers - sum_squares;
+        int sum_squares = 0;
+        int sum_numbers = 0;
+        for (int i=1;i<=100;i++) {
+            sum_squares = sum_squares + i * i;
+            sum_numbers = sum_numbers + i;
+        }
+        return sum_numbers * sum_numbers - sum_squares;
     }
 
     /* Euler 7
      * What is the 10,001st prime number?
      */
     public int euler7() {
-	ArrayList<Integer> primes = new ArrayList<Integer>();
-	primes.add(3);
-	int n = 5;
-	while (primes.size() <= 10001) {
-	    boolean notPrime = false;
-	    for (int current : primes) {
-		if (n % current == 0) {
-		    notPrime = true;
-		    n += 2;
-		    break;
-		}
-	    }
-	    if (notPrime) continue;
-	    primes.add(n);
-	    n += 2;
-	}
-	return primes.get(9999);
+        ArrayList<Integer> primes = new ArrayList<Integer>();
+        primes.add(3);
+        int n = 5;
+        while (primes.size() <= 10001) {
+            boolean notPrime = false;
+            for (int current : primes) {
+                if (n % current == 0) {
+                    notPrime = true;
+                    n += 2;
+                    break;
+                }
+            }
+            if (notPrime) continue;
+            primes.add(n);
+            n += 2;
+        }
+        return primes.get(9999);
+    }
+
+    private List<Integer> findPrimes(int n) {
+        List<Integer> primes = new ArrayList<Integer>();
+        primes.add(2);
+        int m = 3;
+        while (m < (n*0.5 +1)) {
+            boolean notPrime = false;
+            for (int current : primes) {
+                if (n % current == 0) {
+                    notPrime = true;
+                    n += 2;
+                    break;
+                }
+            }
+            if (notPrime) continue;
+            primes.add(m);
+            m += 2;
+        }
+        return primes;
     }
 
     private int subEuler8(ArrayList<Integer> lst) {
-	int returnValue = 0;
-	for (int i=0;i<5;i++) {
-	    returnValue += lst.get(i);
-	}
-	return returnValue;
+        int returnValue = 0;
+        for (int i=0;i<5;i++) {
+            returnValue += lst.get(i);
+        }
+        return returnValue;
     }
 
     /* Euler 8
@@ -164,40 +184,62 @@ public class Euler {
      * the regexp -> 'a,'bs/\(\d\)/\1,/g
      */
     public int euler8() {
-	String sequenceOfNumbers = "73167176531330624919225119674426574742355349194934"+
-	    "96983520312774506326239578318016984801869478851843"+
-	    "85861560789112949495459501737958331952853208805511"+
-	    "12540698747158523863050715693290963295227443043557"+
-	    "66896648950445244523161731856403098711121722383113"+
-	    "62229893423380308135336276614282806444486645238749"+
-	    "30358907296290491560440772390713810515859307960866"+
-	    "70172427121883998797908792274921901699720888093776"+
-	    "65727333001053367881220235421809751254540594752243"+
-	    "52584907711670556013604839586446706324415722155397"+
-	    "53697817977846174064955149290862569321978468622482"+
-	    "83972241375657056057490261407972968652414535100474"+
-	    "82166370484403199890008895243450658541227588666881"+
-	    "16427171479924442928230863465674813919123162824586"+
-	    "17866458359124566529476545682848912883142607690042"+
-	    "24219022671055626321111109370544217506941658960408"+
-	    "07198403850962455444362981230987879927244284909188"+
-	    "84580156166097919133875499200524063689912560717606"+
-	    "05886116467109405077541002256983155200055935729725"+
-	    "71636269561882670428252483600823257530420752963450";
-	String[] ary = sequenceOfNumbers.split("(?!^)");
-	ArrayList<Integer> numberList = new ArrayList<>();
-	for (int i=0;i<ary.length;i++) {
-	    numberList.add(new Integer(ary[i]));
-	}
-	int greatest = subEuler8(numberList);
-	while (true) {
-	    numberList.remove(0);
-	    if (numberList.size() == 4)
-		break;
-	    if (subEuler8(numberList) > greatest)
-		greatest = subEuler8(numberList);
-	}
-	return greatest;
+        String sequenceOfNumbers = 
+            "73167176531330624919225119674426574742355349194934"+
+            "96983520312774506326239578318016984801869478851843"+
+            "85861560789112949495459501737958331952853208805511"+
+            "12540698747158523863050715693290963295227443043557"+
+            "66896648950445244523161731856403098711121722383113"+
+            "62229893423380308135336276614282806444486645238749"+
+            "30358907296290491560440772390713810515859307960866"+
+            "70172427121883998797908792274921901699720888093776"+
+            "65727333001053367881220235421809751254540594752243"+
+            "52584907711670556013604839586446706324415722155397"+
+            "53697817977846174064955149290862569321978468622482"+
+            "83972241375657056057490261407972968652414535100474"+
+            "82166370484403199890008895243450658541227588666881"+
+            "16427171479924442928230863465674813919123162824586"+
+            "17866458359124566529476545682848912883142607690042"+
+            "24219022671055626321111109370544217506941658960408"+
+            "07198403850962455444362981230987879927244284909188"+
+            "84580156166097919133875499200524063689912560717606"+
+            "05886116467109405077541002256983155200055935729725"+
+            "71636269561882670428252483600823257530420752963450";
+        String[] ary = sequenceOfNumbers.split("(?!^)");
+        ArrayList<Integer> numberList = new ArrayList<>();
+        for (int i=0;i<ary.length;i++) {
+            numberList.add(new Integer(ary[i]));
+        }
+        int greatest = subEuler8(numberList);
+        while (true) {
+            numberList.remove(0);
+            if (numberList.size() == 4)
+                break;
+            if (subEuler8(numberList) > greatest)
+                greatest = subEuler8(numberList);
+        }
+        return greatest;
     }
-
+    
+    /*
+     * There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+     * Find the product abc.
+     * Euclid's formula : a = m*m - n*n, b=2mn, c = m*m+n*n
+     */
+    public Set<Integer> euler9() {
+        int sum = 1000;
+        Set<Integer> set = new HashSet<>();
+        for (int x=1;x<Math.ceil(sum / 3);x++) {
+            for (int y=x+1;y<=sum+x;y++) {
+                int z = sum - x - y;
+                if (z * z == x * x + y * y) {
+                    set.add(x);
+                    set.add(y);
+                    set.add(z);
+                    break;
+                }
+            }
+        }
+        return set;
+    }
 }//end Euler
--- src/RunEuler.java
+++ src/RunEuler.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class RunEuler {
     public static void main(String args[]) {
         Euler e = new Euler();
@@ -6,8 +8,12 @@ public class RunEuler {
         System.out.println("3. The largest prime factor is " + e.euler3());
         System.out.println("4. The largest palindrome is " + e.euler4());
         System.out.println("5. The smallest number is " + e.euler5());
-	System.out.println("6. The difference is " + e.euler6());
-	System.out.println("7. The 10,001st prime is " + e.euler7());
-	System.out.println("8. The largest product is " + e.euler8());
+        System.out.println("6. The difference is " + e.euler6());
+        System.out.println("7. The 10,001st prime is " + e.euler7());
+        System.out.println("8. The largest product is " + e.euler8());
+        System.out.println("9. The triplet is :");
+        for (Integer i: e.euler9()) {
+            System.out.println(i);
+        }
     }
 }
